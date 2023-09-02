0b9bf1e4aaaef5c4eab5eaec263f7a23f95793e3
Hector <torrezhectorb@gmail.com>
2023-09-01 19:33:27 -0600

    add methods

--- server.js
+++ server.js
@@ -1,16 +1,47 @@
 import 'dotenv/config'
 import express from 'express';
 import mongoose from 'mongoose'
+import cors from 'cors'
 import { connectDB } from './config/connectDB.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000
-
 connectDB()
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+
+const catBookSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    favoriteFood: String,
+    funFact: String,
+    image: String,
+})
+
+const CatBook = mongoose.model('CatBook', catBookSchema)
+
+app.get('/catBook', async (req, res) => {
+    const cats = await CatBook.find()
+    res.send(cats)
+})
+
+app.get('/catBook/:id', async (req, res) => {
+    const cat = await CatBook.findById()
+    res.send(cat)
+})
+
+app.post('/catBook', async (req, res) => {
+    const newCat = new CatBook(req.body)
+    const saveCat = await newCat.save()
+    res.send(saveCat)
+})
+
+app.delete('/catBook/:id', async(req, res)=>{
+    await CatBook.findByIdAndDelete(req.params.id)
+    res.status(200).send('Cat deleted')
+})
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB')
